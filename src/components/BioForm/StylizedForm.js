ae1669686925610b3e63e6fe7c6844daf86a2d1d
gloss-water <gloss.water@gmail.com>
2018-03-24 18:52:37 -0600

    finish tying template data to state data

--- src/components/BioForm/StylizedForm.js
+++ src/components/BioForm/StylizedForm.js
@@ -13,54 +13,66 @@ const StylizedForm = ({ data }) => (
                 </tr>
                 <tr>
                     <td className="app">{data.gender}</td>
-                    <td className="app">affiliation</td>
+                    <td className="app">{data.aff}</td>
                 </tr>
                 <tr>
-                    <td className="app">sexuality </td>
-                    <td className="app">occupation</td>
+                    <td className="app">{data.sexuality} </td>
+                    <td className="app">{data.occupation}</td>
                 </tr>
             </tbody>
         </table> 
         <div className="cat2">APPEARANCE • 
-            <div className="cat3">FACECLAIM: CHARACTER, SERIES </div>
+            <div className="cat3">FACECLAIM: {data.faceclaim} </div>
         </div>
         <table className="traits">
             <tbody>
                 <tr>
                     <td className="header">pictures </td>
                     <td className="list">
-                        <a href="LINK FOR PIC #1">one</a>
-                        <a href="LINK FOR PIC #2">two</a>
-                        <a href="LINK FOR PIC #3">three</a>
+                        <a href={data['image one']}>one</a> { ' ' }
+                        <a href={data['image two']}>two</a> { ' ' }
+                        <a href={data['image three']}>three</a>
                     </td>
                 </tr>
             </tbody>
         </table>
-        <p> text </p>
+        <p>{data.appearance}</p>
         <div className="cat4">• PERSONALITY </div>
         <table className="traits">
             <tbody>
                 <tr>
                     <td className="header">positive </td>
-                    <td className="list"> positive trait 1, positive 2, positive 3, positive 4, positive 5</td>
+                    <td className="list"> 
+                        {data['positive trait 1']} { ' ' }
+                        {data['positive trait 2']} { ' ' }
+                        {data['positive trait 3']} { ' ' }
+                        {data['positive trait 4']} { ' ' }
+                        {data['positive trait 5']} { ' ' }
+                    </td>
                 </tr>
                 <tr>
                     <td className="header">negative <br /></td>
-                    <td className="list">negative trait 1, negative 2, negative 3, negative 4, negative 5</td>
+                    <td className="list">
+                        {data['negative trait 1']} { ' ' }
+                        {data['negative trait 2']} { ' ' }
+                        {data['negative trait 3']} { ' ' }
+                        {data['negative trait 4']} { ' ' }
+                        {data['negative trait 5']} { ' ' }
+                    </td>
                 </tr>
             </tbody>
         </table> 
-        <p>text</p>
+        <p>{data.personality}</p>
         <div className="cat2">HISTORY •  </div>
          <table className="traits">
              <tbody>
                  <tr>
                      <td className="header">hometown </td>
-                     <td className="list"> <i>town,</i> region  </td>
+                     <td className="list"> <i>{data.hometown}</i> </td>
                  </tr>
              </tbody>
          </table>
-         <p>text</p>
+         <p>{data.history}</p>
         <div className="cat4">• POKEMON </div>
         <div className="pokemon">characters may start with 3 pokemon up to level 30. these pokemon cannot be bred, traded, or sold. if your character has a pokemon over level 20, they cannot get a starter from one of the labs.
             <br /><br />
@@ -72,11 +84,11 @@ const StylizedForm = ({ data }) => (
             <b>ATTACKS:</b> up to 6 moves <br />
         </div>
         <div className="rpbox"> <div className="rptext">ABOUT THE RPER</div> 
-            <b>alias:</b> name you go by<br />
-            <b>gender:</b> your gender, as well as your preferred pronouns <br />
-            <b>age:</b> in years <br />
-            <b>character number:</b> is this your first character? your second? <br />
-            <b>where did you find us?:</b> was it an ad, affiliate, word of mouth?<br />
+            <b>alias:</b> {data.alias}<br />
+            <b>gender:</b> {data['gender/Pronouns']}<br />
+            <b>age:</b> {data['your age']} <br />
+            <b>character number:</b> {data['character number']}<br />
+            <b>where did you find us?:</b> {data['where did you find us?']}<br />
         </div>
     </div>
 );
@@ -87,7 +99,8 @@ StylizedForm.defaultProps = {
         icon: 'http://i.imgur.com/5Rcbl.png',
         age: 'age',
         bday: 'birthday',
-        gender: 'gender'
+        gender: 'gender',
+        [`gender/Pronouns`]: 'your gender, as well as your preferred pronouns'
     }
 }
 
