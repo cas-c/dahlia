9d7313a15507ebe7f6a04fee1693974dff18977a
gloss-water <gloss.water@gmail.com>
2018-06-13 11:06:09 -0600

    unregister service workers due to index.html caching issue

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 import "react-select/dist/react-select.css";
 import "react-virtualized/styles.css";
 import "react-virtualized-select/styles.css";
 
+// import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
+unregister();
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
