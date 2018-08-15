03eb24e7cf31fc65088ffd04bae4b9e1099ec49b
gloss-water <gloss.water@gmail.com>
2018-08-15 12:12:20 -0600

    search by route as well

--- src/components/Finder/index.js
+++ src/components/Finder/index.js
@@ -5,19 +5,27 @@ import pokemon from '../../lib/pokemon';
 import './index.css';
 import Pokemon from './Pokemon';
 import allAvailable from './allAvailablePokemon';
+import { routes } from './routeData';
 
-const options = Array.from(pokemon.all(), (_, index) => ({
-  label: _,
-  value: index
-})).filter(p => allAvailable.includes(p.label));
+const pokemonAsOptions = Array.from(pokemon.all(), (_, index) => ({
+    label: _,
+    value: index
+}));
+const options = pokemonAsOptions.filter(p => allAvailable.includes(p.label));
 
+const routeOptions = routes.map((r, i) => ({
+    label: r.name,
+    value: i,
+    pokemon: pokemonAsOptions.filter(p => r.pokemon.includes(p.label))
+}));
 
 class Finder extends React.Component {
     constructor() {
         super();
         this.state = {
             searchTerm: '',
-            selected: []
+            selected: [],
+            selectedRoute: []
         };
     }
 
@@ -29,6 +37,11 @@ class Finder extends React.Component {
     select = selected => {
         this.setState({ selected });
         localStorage.setItem('selectedPokemon', JSON.stringify(selected));
+    }
+
+    selectRoute = selectedRoute => {
+        this.setState({ selectedRoute });
+        localStorage.setItem('selectedRoute', JSON.stringify(selectedRoute));
     }
 
     render() {
@@ -45,6 +58,21 @@ class Finder extends React.Component {
                 {
                     this.state.selected.map(p => {
                         return <Pokemon key={p.value} data={p} />
+                    })
+                }
+                <hr />
+                <h1>or, let's look at some routes</h1>
+                <VirtualizedSelect
+                    multi={true}
+                    value={this.state.selectedRoute}
+                    options={routeOptions}
+                    onChange={this.selectRoute}
+                />
+                {
+                    this.state.selectedRoute.map(r => {
+                        return r.pokemon.map(p => {
+                            return <Pokemon key={p.value} data={p} />
+                        });
                     })
                 }
             </div>
