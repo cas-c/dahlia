db61289f0b731c1427b3b1bb14c280398e1f8dc7
gloss-water <gloss.water@gmail.com>
2018-08-15 19:26:51 -0600

    code splitting!

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
+
 import './App.css';
 import Home from './components/Home';
-import BioForm from './components/BioForm';
-import Finder from './components/Finder';
 
-import { HashRouter as Router, Route } from 'react-router-dom';
-import XPCalculator from './components/XPCalculator';
+const Loading = props => {
+    if (props.error) {
+        return null; // add error handling
+    } else if (props.pastDelay) {
+        return <div>Loading...</div>;
+    } else {
+        return null;
+    }
+}
+
+const LoadableBioForm = Loadable({
+    loader: () => import('./components/BioForm'),
+    loading: Loading
+});
+
+const LoadableFinder = Loadable({
+    loader: () => import('./components/Finder'),
+    loading: Loading
+});
+
+const LoadableXPCalculator = Loadable({
+    loader: () => import('./components/XPCalculator'),
+    loading: Loading
+});
 
 const App = () => (
     <Router>
         <div className='App'>
             <div>
                 <Route exact path='/' component={Home} />
-                <Route path='/bio' component={BioForm} />
-                <Route path='/finder' component={Finder} />
-                <Route path='/xp' component={XPCalculator} />
+                <Route path='/bio' component={LoadableBioForm} />
+                <Route path='/finder' component={LoadableFinder} />
+                <Route path='/xp' component={LoadableXPCalculator} />
             </div>
         </div>
     </Router>
